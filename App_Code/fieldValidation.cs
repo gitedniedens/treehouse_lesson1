4fcd788f847f5c7608ae2e370ea38d6ad9e6e5be
Edward Niedens <42617741+gitedniedens@users.noreply.github.com>
2018-09-18 14:12:34 -0400

    added utility functions

---
+++ App_Code/fieldValidation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// Validation of fields and types for application
+/// </summary>
+public class fieldValidation
+{
+
+    // positive number with decimal allowed
+    public static bool isMoney(string text)
+    {
+        Regex regex = new Regex(@"^[+]?[0-9]*\.?[0-9]+$");
+        return regex.IsMatch(text);
+    }
+
+    // Match all alphanumeric character and predefined wild characters '@*#.'. Password must be between 6 and 50 characters.
+    public static bool isPassword(string text)
+    {
+        Regex regex = new Regex(@"^([a-zA-Z0-9@*#.]{6,50})$");
+        return regex.IsMatch(text);
+    }
+
+    // E-mail matches: username@domain.com | u-s_e.r1@s-ub2.domain-name.museum:8080 | user_name@123.123.123.12
+    public static bool isEmail(string text)
+    {
+        Regex regex = new Regex(@"^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$");
+        return regex.IsMatch(text);
+    }
+
+    // File Name Validator: only allow a-z, A-Z, 0-9, underscores, hyphens, decimals, and spaces.
+    public static bool HC_isValidFileName(string text)
+    {
+        //Regex regex = new Regex(@"^[a-zA-Z0-9_\s-]+$");
+        Regex regex = new Regex(@"^([a-zA-Z0-9_\s-.]{3,200})$");
+        return regex.IsMatch(text);
+    }
+
+
+}
---
+++ App_Code/stringFormatting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// String Formatting Classes For Use Across Application
+/// </summary>
+public class stringFormatting
+{
+
+    // replace single and double quote, also less than and greater than sign for HTML
+	public static string filterInput(string inputString)
+	{
+
+        string outputString;
+
+
+        outputString = inputString.Replace("'", "");
+        outputString = outputString.Replace("\"", "");
+        outputString = outputString.Replace("<", "");
+        outputString = outputString.Replace(">", "");
+
+
+        return outputString;
+
+	}
+
+
+
+}
